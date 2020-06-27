837de9e67e578126d94c9b593aeaf34914839c49
Ruairi O Brien <ruairitobrien@gmail.com>
2020-06-27 20:31:14 +0100

    Added basic auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './assets/img/3d-stocktake-logo.jpg'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import axios from 'axios'
+import './App.css'
+import Auth from './Auth'
+
+const getFirebaseConfig = new Promise((resolve, reject) => {
+  axios
+    .get(`/__/firebase/init.json`)
+    .then((res) => {
+      resolve(res.data)
+    })
+    .catch((err) => reject(err))
+})
 
 function App() {
+  const [initialized, setInitialized] = React.useState(false)
+
+  React.useEffect(() => {
+    getFirebaseConfig.then((result: any) => {
+      firebase.initializeApp(result)
+      setInitialized(true)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section>{initialized && <Auth />}</section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Auth.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import * as firebase from 'firebase/app'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
+function Auth() {
+  const [loading, setLoading] = React.useState(true)
+  const [isSignedIn, setIsSignedIn] = React.useState(true)
+
+  React.useEffect(() => {
+    console.log(firebase.auth().currentUser)
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => setIsSignedIn(!!user))
+
+    return unregisterAuthObserver
+  }, [])
+
+  const uiConfig = {
+    callbacks: {
+      signInSuccess: (auth: any) => {
+        console.log(auth)
+      },
+      signInSuccessWithAuthResult: () => false,
+      uiShown: function () {
+        setLoading(false)
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: false
+      },
+      // List of OAuth providers supported.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ]
+  }
+
+  return (
+    <div>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      {loading && <p>Loading....</p>}
+      {isSignedIn && (
+        <div>
+          <p>
+            Hello {firebase.auth().currentUser?.displayName}! You are a....
+            poopyface
+          </p>
+          <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Auth
